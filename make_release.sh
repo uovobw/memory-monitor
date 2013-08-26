70fcd865cee37864e6ca1338161079802ca18dea
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-08-26 09:15:38 +0200

    Update release script

--- make_release.sh
+++ make_release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FILES="psutil monitor.py 32bitlib/_psutil_linux.so 32bitlib/_psutil_posix.so memory_monitor.yaml"
+FILES="psutil yaml monitor.py 32bitlib/_psutil_linux.so 32bitlib/_psutil_posix.so memory_monitor.yaml"
 TMPDIR="memory_monitor"
 
 mkdir $TMPDIR
-cp -a $FILES $TMPDIR
+cp -av $FILES $TMPDIR
 
 tar zcfp memory_monitor.tar.gz $TMPDIR
 
